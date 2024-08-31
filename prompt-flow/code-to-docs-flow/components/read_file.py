c75e696b9a150a69e576d739591c11534f60548b
Y. Saito <49590084+yus04@users.noreply.github.com>
2024-08-31 05:38:01 +0000

    update read_file.py

--- prompt-flow/code-to-docs-flow/components/read_file.py
+++ prompt-flow/code-to-docs-flow/components/read_file.py
@@ -1,28 +1,12 @@
 from promptflow import tool
-from azure.storage.blob import BlobServiceClient
 from storage.account import StorageAccount
 
 @tool
 def read_file(storage_account: dict) -> str:
-    # ストレージアカウント情報
+    # ストレージアカウント
     sa = StorageAccount.from_dict(storage_account)
-    connection_string = sa.get_connection_string()
-    container_name = sa.get_container_name()
-    blob_name = sa.get_blob_name()
 
-    # BlobServiceClient の作成
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-    # コンテナクライアントの取得
-    container_client = blob_service_client.get_container_client(container_name)
-
-    # Blob クライアントの取得
-    blob_client = container_client.get_blob_client(blob_name)
-
-    # Blob データのダウンロード
-    downloaded_blob = blob_client.download_blob().readall()
-
-    # Blob データのデコード
-    file_content = downloaded_blob.decode('shift_jis')
+    # Blob データのダウンロードとデコード
+    file_content = sa.download_blob().decode('shift_jis')
 
     return file_content
