4ed542bdff50d2da877485eeb0bc0fc00ca24c26
Y. Saito <49590084+yus04@users.noreply.github.com>
2024-09-02 13:50:31 +0000

    add procedure div operation

---
+++ prompt-flow/code-to-docs-flow/components/gen_procedure_div.py
@@ -0,0 +1,18 @@
+from promptflow.core import tool
+from utils.extract_code import ExtractCode
+from utils.split_code import SplitCode
+from utils.get_env import GetEnv
+from ai.aoaiclient import AOAIClient
+
+@tool
+def gen_procedure_div(system_message: str, user_message: str) -> str:
+    extracted_code = user_message
+    sc = SplitCode(extracted_code)
+    splited_code = sc.split_by_entry()
+    ge = GetEnv()
+    aoaic = AOAIClient(ge.service_name(), ge.api_version(), ge.token(), ge.deployment())
+    docs = []
+    for code in splited_code:
+        doc = aoaic.chat_completions_create(system_message, code)
+        docs.append(doc)
+    return '\n'.join(docs)
