f6817e680859432765cf32a25dc317bbe63ac00a
Y. Saito <49590084+yus04@users.noreply.github.com>
2024-09-03 04:49:38 +0000

    add header for output document

--- prompt-flow/code-to-docs-flow/components/gen_procedure_div.py
+++ prompt-flow/code-to-docs-flow/components/gen_procedure_div.py
@@ -11,8 +11,13 @@ def gen_procedure_div(system_message: str, user_message: str) -> str:
     splited_code = sc.split_by_entry()
     ge = GetEnv()
     aoaic = AOAIClient(ge.service_name(), ge.api_version(), ge.token(), ge.deployment())
-    docs = []
+    docs = add_header([])
     for code in splited_code:
         doc = aoaic.chat_completions_create(system_message, code)
         docs.append(doc)
     return '\n'.join(docs)
+
+def add_header(item: list[str]) -> list[str]:
+    item.append("| 処理概要 | コード | 処理説明 |")
+    item.append("|---|---|---|")
+    return item
--- prompt-flow/code-to-docs-flow/components/merge_file.py
+++ prompt-flow/code-to-docs-flow/components/merge_file.py
@@ -2,4 +2,12 @@ from promptflow.core import tool
 
 @tool
 def merge_file(identification_docs: str, environment_docs: str, data_docs: str, procedure_docs: str) -> str:
+    identification_docs = add_heading("Identification", identification_docs)
+    environment_docs = add_heading("Environment", environment_docs)
+    data_docs = add_heading("Data", data_docs)
+    procedure_docs = add_heading("Procedure", procedure_docs)
     return '\n'.join([identification_docs, environment_docs, data_docs, procedure_docs])
+
+def add_heading(div_type: str, docs: str) -> str:
+    header = f"# {div_type} Division の説明\n"
+    return '\n'.join([header, docs])
